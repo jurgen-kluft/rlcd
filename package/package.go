25afac08cfce504b1c01bbae6e02207a89b53a05
jurgen.kluft <jurgen.kluft@gmail.com>
2026-05-11 08:25:59 +0800

    * rlcd
    * dwo; AMOLED support, DWO boards
    * wcs; LCD support, WCS board

--- package/package.go
+++ package/package.go
@@ -2,6 +2,7 @@ package rlcd
 
 import (
 	denv "github.com/jurgen-kluft/ccode/denv"
+	cunittest "github.com/jurgen-kluft/cunittest/package"
 	rcore "github.com/jurgen-kluft/rcore/package"
 )
 
@@ -14,18 +15,43 @@ func GetPackage() *denv.Package {
 	name := repo_name
 
 	// dependencies
+	cunittestpkg := cunittest.GetPackage()
 	corepkg := rcore.GetPackage()
-
-	// TODO make a library per LCD type
 
 	// main package
 	mainpkg := denv.NewPackage(repo_path, repo_name)
 	mainpkg.AddPackage(corepkg)
+	mainpkg.AddPackage(cunittestpkg)
+
+	// TODO, make a library per sensor ?
 
 	// main library
-	mainlib := denv.SetupCppLibProject(mainpkg, name)
-	mainlib.AddDependencies(corepkg.GetMainLib())
+	mainLib := denv.SetupCppLibProject(mainpkg, name)
+	mainLib.AddDependencies(corepkg.GetMainLib())
 
-	mainpkg.AddMainLib(mainlib)
+	// test library
+	mainTestLib := denv.SetupCppTestLibProject(mainpkg, name)
+	mainTestLib.AddDependencies(corepkg.GetTestLib())
+
+	// DWO library
+	dwoLib := denv.SetupCppLibraryForArduinoEsp32(mainpkg, "lib_dwo", "dwo")
+	dwoLib.AddDependencies(corepkg.GetMainLib())
+
+	// WCS library
+	wcsLib := denv.SetupCppLibraryForArduinoEsp32(mainpkg, "lib_wcs", "wcs")
+	wcsLib.AddDependencies(corepkg.GetMainLib())
+
+	// unittest project
+	maintest := denv.SetupCppTestProject(mainpkg, name)
+	maintest.AddDependencies(cunittestpkg.GetMainLib())
+	maintest.AddDependency(mainTestLib)
+
+	mainpkg.AddMainLib(mainLib)
+	mainpkg.AddLibrary(dwoLib)
+	mainpkg.AddLibrary(wcsLib)
+
+	mainpkg.AddTestLib(mainTestLib)
+	mainpkg.AddUnittest(maintest)
+
 	return mainpkg
 }
